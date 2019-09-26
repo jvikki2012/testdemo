f6ffc648dfbc440c5a4433e3a4b89530ce5b327e
Vignesh <jvignesh2012@gmail.com>
2019-09-26 16:32:59 +0530

    adding new commit

--- SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/Browsertest.java
+++ SeleniumFramework/src/test/java/SeleniumFramework/SeleniumFramework/Browsertest.java
@@ -2,18 +2,24 @@ package SeleniumFramework.SeleniumFramework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class Browsertest {
 	
 	public static void main(String[] args) {
 		
-		
-		System.setProperty("webdriver.chrome.driver", "D:\\Eclipse workspace\\testdemo\\SeleniumFramework\\drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		//String path=System.getProperty("user.dir");
+		//System.setProperty("webdriver.chrome.driver", path+"\\drivers\\chromedriver.exe");
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
 		driver.get("https://google.com");
+	
 		System.out.println("Hello world");
 		System.out.println("Step completed");
+		System.out.println("Step completed1");
 		
 	}
 
